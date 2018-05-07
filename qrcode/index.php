53c1dcd72d02078b54d93a98b09454b21b934fcc
unknown <d.aji.apriyadi@gmail.com>
2018-05-08 05:36:48 +0700

    commit

---
+++ qrcode/index.php
@@ -0,0 +1,16 @@
+<?php
+include "phpqrcode/qrlib.php";
+
+QRcode::png("http://www.erllang.ga", "code.png", "L", 4, 4);
+QRcode::png("Muhammad", "code1.png", "L", 6, 4);
+QRcode::png("Erlangga", "code2.png", "L", 8, 4);
+
+echo "<center>";
+echo "Textnya: http://www.erllang.ga<img src='code.png' /><br/>";
+
+echo "Textnya: Muhammad<img src='code1.png' /><br/>";
+
+echo "Textnya: Erlangga<img src='code2.png' /><br/>";
+echo "</center>";
+
+?>
